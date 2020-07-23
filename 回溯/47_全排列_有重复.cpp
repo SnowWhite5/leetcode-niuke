ad295041147000aecea564886884af57082cb77e
SnowWhite5 <44915205+SnowWhite5@users.noreply.github.com>
2020-07-23 22:17:49 +0800

    Add files via upload

---
+++ 回溯/47_全排列_有重复.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<algorithm>
+#include<unordered_set>
+using namespace std;
+
+void swap(vector<int>& n,int i,int j)
+{
+    int t=n[i];
+    n[i]=n[j];
+    n[j]=t;
+}
+void help(vector<vector<int>>& ans,vector<int>& n,int k)
+{
+    if(k==n.size()) ans.push_back(n);
+    unordered_set<int> dict;
+    for(int i=k;i<n.size();++i)
+    {
+        if(dict.count(n[i])) continue;
+        dict.insert(n[i]);
+        swap(n,i,k);
+        help(ans,n,k+1);
+        swap(n,i,k);
+    }
+}
+int main(){
+    int n,t;
+    vector<int> nums;
+    scanf("%d",&n);
+    t=n;
+    while(t--)
+    {
+        int i;
+        scanf("%d",&i);
+        nums.push_back(i);
+    }
+    vector<vector<int>> ans;
+    help(ans,nums,0);
+    for(int i=0;i<ans.size();++i)
+    {
+        for(int j=0;j<nums.size();++j) printf("%d ",ans[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
+
